74f412158e6ec0b6060d2f101a01cbe753a11c5a
Sven Reiter <139964659+svenson2907@users.noreply.github.com>
2023-08-08 13:08:06 +0200

    Update CaseStudyController.java

--- DB_CaseStudy/src/main/java/db/caseStudy/CaseStudyController.java
+++ DB_CaseStudy/src/main/java/db/caseStudy/CaseStudyController.java
@@ -1,6 +1,11 @@
 package db.caseStudy;
 
 import org.springframework.web.bind.annotation.*;
+import org.xml.sax.SAXException;
+import readFromXml.Start;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.util.Map;
 
 @RestController
@@ -11,15 +16,16 @@ public class CaseStudyController {
         this.repository = repository;
     }
 
-    @RequestMapping(path = "/trackSectionx/{ril100}/{trainNumber}/{number}", method = RequestMethod.GET)
-    String getTrackSection(@PathVariable String ril100, @PathVariable Integer trainNumber, @PathVariable Integer number) {
-        // http://localhost:8080/trackSectionx/ril100/1/2
-        return ("It Works! ril100:="+ril100+" trainNumber:="+trainNumber+" waggon:="+number);
+    @RequestMapping(path = "/trackSections/{ril100}/{trainNumber}/{number}", method = RequestMethod.GET)
+    String getTrackSection(@PathVariable String ril100, @PathVariable Integer trainNumber, @PathVariable Integer number) throws IOException, ParserConfigurationException, SAXException {
+        // http://localhost:8080/trackSections/ril100/1/2
+        Start start = new Start();
+        return start.run(""+trainNumber, ""+number);
     }
-
+/*
     @RequestMapping(value="/trackSections", method = RequestMethod.GET)
     String getParams(@RequestParam Map<String, String> params ) {
         // http://localhost:8080/trackSections?station=ril100&train=1&waggon=2
         return params.toString();
-    }
+    }*/
 }
