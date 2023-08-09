a7b6060efc04a1aeb16c8562147d91752642b093
Sven Reiter <139964659+svenson2907@users.noreply.github.com>
2023-08-09 14:28:48 +0200

    Update ReadFromXml.java
    Adds comments;
    Removes <waggonNumberPasst>;

--- DB_CaseStudy/src/main/java/services/ReadFromXml.java
+++ DB_CaseStudy/src/main/java/services/ReadFromXml.java
@@ -15,13 +15,13 @@ public class ReadFromXml {
         Document doc = builder.parse("src\\main\\resources\\FF_2017-12-01_10-47-17.xml");
         doc.getDocumentElement().normalize();
         String section="";
-        boolean trainNumberPasst=false, waggonNumberPasst=false;
+        boolean trainNumberPasst=false;
         for(int t=0; t<doc.getElementsByTagName("train").getLength(); ++t) {    //iterates indices of descendant elements to train-Tag
             trainNumberPasst = false;
             for(int i=0; i<doc.getElementsByTagName("train").item(t).getChildNodes().getLength(); ++i) {    //iterates indices of descendant elements to train-Tags t-th child
                 if(!doc.getElementsByTagName("train").item(t).getChildNodes().item(i).getNodeName().replaceAll("\\s","").equals("#text")) {    //checks i-th element for redundant "#text"-element
                     if(doc.getElementsByTagName("train").item(t).getChildNodes().item(i).getTextContent().replaceAll("\\s", "").equals(trainNumber)) {    //check i-th element whether it is the searched train number
-                        trainNumberPasst = true;
+                        trainNumberPasst = true;    //to remember that trainNumber got found
                     }
                     if(doc.getElementsByTagName("train").item(t).getChildNodes().item(i).getNodeName().replaceAll("\\s", "").equals("waggons")) {    //checks i-th element for "waggons"-tag
                         for(int w=0; w<doc.getElementsByTagName("train").item(t).getChildNodes().item(i).getChildNodes().getLength(); ++w) {    //iterates indices of descendant elements to train-Tags t-th and of them i-th children
@@ -36,12 +36,9 @@ public class ReadFromXml {
                                     }
                                     if(doc.getElementsByTagName("train").item(t).getChildNodes().item(i).getChildNodes().item(w).getChildNodes().item(j).getNodeName().replaceAll("\\s", "").equals("number") &&           //checks if j-th NodeName equals to "number" ANS
                                        doc.getElementsByTagName("train").item(t).getChildNodes().item(i).getChildNodes().item(w).getChildNodes().item(j).getTextContent().replaceAll("\\s", "").equals(waggonNumber)) {    //j-th text equals to waggonNumber
-                                        waggonNumberPasst = true;
                                         if(trainNumberPasst) {
                                             return "{ \"sections\": [\""+section+"\"]}";    //if so section got found
                                         }
-                                    }else {
-                                        waggonNumberPasst = false;
                                     }
                                 }
                             }
